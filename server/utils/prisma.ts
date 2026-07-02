01c122e78c77949cea56e3d8ba812bc187a9ab33
Nur Aziz <sukabaru09@gmail.com>
2026-07-02 11:10:33 +0700

    fix(prisma): disable rejectUnauthorized on tls

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -2,7 +2,12 @@ import '@dotenvx/dotenvx/config';
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../prisma/generated/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL;
 
-const adapter = new PrismaPg({ connectionString });
+const connectionOpt = { connectionString };
+if(connectionString && connectionString.includes('sslmode')){
+  connectionOpt.ssl = { rejectUnauthorized: false }
+}
+
+const adapter = new PrismaPg(connectionOpt);
 export const prisma = new PrismaClient({ adapter });
